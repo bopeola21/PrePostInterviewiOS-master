d427cc03c6c52c885bdb90eb252d9c35004023f5
bopeola21 <bopeola@Jides-MacBook-Air.local>
2015-03-06 21:26:39 -0500

    Newfile

---
+++ Pre-Post Interview/Pre-Post Interview/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pre-Post Interview
+//
+//  Created by William Judd on 3/6/15.
+//  Copyright (c) 2015 William Judd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,1 @@
+# PrePostInterview---iOS
